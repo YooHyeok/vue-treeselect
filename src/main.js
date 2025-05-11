f2112142d9b6dd6b09fc0d4c4d9440f4b4116504
webdevyoo <yjk7454@naver.com>
2025-05-12 01:13:17 +0900

    feat: `main.js` - @riophae/vue-treeselect 라이브러리 전역 컴포넌트(TreeSelect) 등록 및 스타일 적용

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Treeselect from '@riophae/vue-treeselect'
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+Vue.component('TreeSelect', Treeselect)
 Vue.config.productionTip = false
 
 new Vue({
