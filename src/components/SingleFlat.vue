d2acc16180e8f30669c63e6ab18444f7cb4ee0f2
webdevyoo <yjk7454@naver.com>
2025-05-14 00:09:24 +0900

    feat: 단일 선택 기본 treeselect 예제 SingleFlat 컴포넌트 구현

---
+++ src/components/SingleFlat.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width:300px">
+    <label>단일 선택 (flat 구조)</label>
+    <TreeSelect
+      v-model="selected"
+      :options="options"
+      placeholder="항목을 선택하세요"
+    />
+    <p>선택된 값: {{ selected }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'SingleFlat',
+  setup() {
+    const selected = ref(null)
+
+    const options = [
+      { id: '1', label: '사과' },
+      { id: '2', label: '바나나' },
+      { id: '3', label: '포도' }
+    ]
+
+    return { selected, options }
+  }
+})
+</script>
