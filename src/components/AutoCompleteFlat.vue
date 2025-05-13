35c3b5eb0bb430c43fcfd885ba5222394bc671d8
webdevyoo <yjk7454@naver.com>
2025-05-14 00:15:33 +0900

    feat: 상수기반 자동완성 기본옵션 구성 treeselect 컴포넌트 구현

---
+++ src/components/AutoCompleteFlat.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="width:300px">
+    <label>자동완성 셀렉트 (flat 구조)</label>
+    <TreeSelect
+      v-model="selected"
+      :options="options"
+      :searchable="true"
+      placeholder="검색해서 선택하세요"
+    />
+    <p>선택된 값: {{ selected }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'AutoCompleteFlat',
+  setup() {
+    const selected = ref(null)
+
+    const options = [
+      { id: 'dog', label: '강아지' },
+      { id: 'dogg', label: '멍멍이' },
+      { id: 'cat', label: '고양이' },
+      { id: 'catt', label: '야옹이' },
+      { id: 'parrot', label: '앵무새' }
+    ]
+
+    return { selected, options }
+  }
+})
+</script>
